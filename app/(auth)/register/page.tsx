ba1d2617782b2036a4ee5e1a01af9cda39113b78
Foxix <fenixoffc@mail.ru>
2024-11-07 06:37:04 +0200

    dude

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -3,9 +3,9 @@
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "../../../components/ui/button"
+import { Input } from "../../../components/ui/input"
+import { Card, CardContent, CardHeader, CardTitle } from "../../../components/ui/card"
 
 export default function RegisterPage() {
   const [username, setUsername] = useState('')
